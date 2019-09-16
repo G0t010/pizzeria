a044508597117c1de9f49835b285baca98ee09ae
Nicolas G <ngeraudie@gmail.com>
2019-09-16 11:03:07 -0400

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/', function(req, res, next) {
+  console.log(req.body)
+  res.render('commande', { title: 'Express', commande:req.body });
+});
+module.exports = router;
